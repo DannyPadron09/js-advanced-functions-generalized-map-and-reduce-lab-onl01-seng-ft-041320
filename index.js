a472920145f525a3959fafded3a440ae0480caaf
dannypadron09 <danny.padron09@gmail.com>
2020-07-30 15:35:40 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Add your functions here
+function map(array, callback) {
+    let newArray = []
+    for (let i = 0; i < array.length; i++) {
+      let item = array[i]
+      newArray.push(callback(item))
+    }
+    return newArray
+  }
+
+function reduce(array, callback, starting) {
+    let item = (!!starting) ? starting : array[0]
+    let i = (!!starting) ? 0 : 1
+
+    for (; i < array.length; i++) {
+        item = callback(array[i], item)
+    }
+    return item 
+}
+  
